125951ce427491fe4261947cf3e87b8502e99c74
hvvrD <hwrdlim@gmail.com>
2020-08-31 12:47:59 +0800

    Add accelerometer controls, add spawn point, and others
    PlayerController
    - accelerometer controls
    - add tap controls, for spawn point
    
    MainGame scene
    - change wall properties
    - change player speed
    - add spawn point

--- Assets/_Completed-Game/Scripts/PlayerController.cs
+++ Assets/_Completed-Game/Scripts/PlayerController.cs
@@ -7,8 +7,18 @@ public class PlayerController : MonoBehaviour {
 	public float speed;
 	public Text countText;
 	public Text winText;
+	[SerializeField] private Transform spawnPoint;
+
 	private Rigidbody rb;
 	private int count;
+
+	//Tap variables
+	private int TapCount;
+	private float NewTime;
+	private float MaxDubbleTapTime = 1f;
+
+	//Debug
+	public Vector3 accelerometer;
 
 	private void Start ()
 	{
@@ -18,14 +28,51 @@ public class PlayerController : MonoBehaviour {
 		winText.text = "";
 	}
 
+	private void Update()
+	{
+		if (Input.touchCount == 1)
+		{
+			Touch touch = Input.GetTouch(0);
+
+			if (touch.phase == TouchPhase.Ended)
+			{
+				TapCount += 1;
+			}
+
+			if (TapCount == 1)
+			{
+
+				NewTime = Time.time + MaxDubbleTapTime;
+			}
+			else if (TapCount == 2 && Time.time <= NewTime)
+			{
+				//Respawn
+				this.transform.position = spawnPoint.position;
+
+				TapCount = 0;
+			}
+
+		}
+		if (Time.time > NewTime)
+		{
+			TapCount = 0;
+		}
+	}
+
 	private void FixedUpdate ()
 	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
-		
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		//TODO: if keyboard
+		//float moveHorizontal = Input.GetAxis ("Horizontal");
+		//float moveVertical = Input.GetAxis ("Vertical");
 
-		rb.AddForce (movement * speed);
+		float moveHorizontal = Input.acceleration.x;
+		float moveVertical = Input.acceleration.y;
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce(movement * speed);
+
+		//Debug
+		accelerometer = Input.acceleration;
 	}
 
 	private void OnTriggerEnter(Collider other) 
